df242fa1e103dd12caf9fe688c8ab93f41964de1
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-06-29 03:06:50 +0530

    LeetCode: Solve #394 Decode String

---
+++ LeetCode/Medium/Decode String/394. Decode String.java
@@ -0,0 +1,45 @@
+// 394. Decode String [Medium]
+// https://leetcode.com/problems/decode-string/
+// Language: java | Runtime: 3 ms | Memory: 43 MB
+// Tags: String, Stack, Recursion
+// Synced: 2026-06-29
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack=new Stack<>();
+        Stack<String> stringStack=new Stack<>();
+        
+        String current="";
+        int number=0;
+
+        for(char ch:s.toCharArray()){
+            if(Character.isDigit(ch)){
+                number=number*10+(ch-'0');
+            }
+            else if(ch=='['){
+                countStack.push(number);
+                stringStack.push(current); //for characters without no. repeats
+
+                number=0;
+                current="";
+            }
+            else if(ch==']'){
+                int repeat=countStack.pop();
+                String previous=stringStack.pop();
+
+                StringBuilder temp=new StringBuilder(previous);
+//ab3[c]
+//Before encountering [:
+//current = "ab";
+                for(int i=0;i<repeat;i++){
+                    temp.append(current);
+                }
+                current=temp.toString();
+            }
+            else{
+                current+=ch;
+            }
+        }
+        return current;
+    }
+}
