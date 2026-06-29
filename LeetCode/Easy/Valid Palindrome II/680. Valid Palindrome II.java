07187b39deb3128b922a2dfafe94b96993056227
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-06-30 01:58:55 +0530

    LeetCode: Solve #680 Valid Palindrome II

---
+++ LeetCode/Easy/Valid Palindrome II/680. Valid Palindrome II.java
@@ -0,0 +1,43 @@
+// 680. Valid Palindrome II [Easy]
+// https://leetcode.com/problems/valid-palindrome-ii/
+// Language: java | Runtime: 4 ms | Memory: 48 MB
+// Tags: Two Pointers, String, Greedy
+// Synced: 2026-06-30
+
+class Solution {
+
+    public boolean validPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+
+            if(s.charAt(left) != s.charAt(right)){
+
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right){
+
+        while(left < right){
+
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
