e07e1c1ad685d7add2a04e7f92ca953b8210ed4a
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-07-11 01:42:06 +0530

    LeetCode: Solve #160 Intersection of Two Linked Lists

---
+++ LeetCode/Easy/Intersection of Two Linked Lists/160. Intersection of Two Linked Lists.java
@@ -0,0 +1,38 @@
+// 160. Intersection of Two Linked Lists [Easy]
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+// Language: java | Runtime: 1 ms | Memory: 52.8 MB
+// Tags: Hash Table, Linked List, Two Pointers
+// Synced: 2026-07-11
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA=headA;
+        ListNode pB=headB;
+
+        while(pA!=pB){
+            if(pA==null){
+                pA=headB;
+            }else{
+                pA=pA.next;
+            }
+
+            if(pB==null){
+                pB=headA;
+            }else{
+                pB=pB.next;
+            }
+        }
+        return pA;
+    }
+}
