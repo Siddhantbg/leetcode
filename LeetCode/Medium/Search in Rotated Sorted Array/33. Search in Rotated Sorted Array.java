782084d10c7c5ec1a60fcc8b2870bcbe1fd83229
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-07-12 22:28:32 +0530

    LeetCode: Solve #33 Search in Rotated Sorted Array

---
+++ LeetCode/Medium/Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,17 @@
+// 33. Search in Rotated Sorted Array [Medium]
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+// Language: java | Runtime: 0 ms | Memory: 43.6 MB
+// Tags: Array, Binary Search
+// Synced: 2026-07-12
+
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+                    return -1;
+
+    }
+}
