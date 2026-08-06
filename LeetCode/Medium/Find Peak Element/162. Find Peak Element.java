a31d2d72129aa9cbb2c742f7e209a65d6aea95c4
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-08-07 02:35:20 +0530

    LeetCode: Solve #162 Find Peak Element

--- LeetCode/Medium/Find Peak Element/162. Find Peak Element.java
+++ LeetCode/Medium/Find Peak Element/162. Find Peak Element.java
@@ -1,8 +1,36 @@
 // 162. Find Peak Element [Medium]
 // https://leetcode.com/problems/find-peak-element/
 // Language: java | Runtime: 0 ms | Memory: 44.2 MB
+// Time:  O(log n)
+// Space: O(1)
 // Tags: Array, Binary Search
 // Synced: 2026-08-07
+//
+// • Binary Search on the slope.
+// 
+// • Compare only:
+//     nums[mid] and nums[mid + 1].
+// 
+// • If nums[mid] < nums[mid + 1]:
+//     You're on an increasing slope.
+//     Search RIGHT.
+// 
+// • If nums[mid] > nums[mid + 1]:
+//     You're on a decreasing slope.
+//     Search LEFT (including mid).
+// 
+// • Use:
+//     while (low < high)
+// 
+// • When low == high,
+//     that index is guaranteed to be a peak.
+// 
+// • Don't use:
+//     high = mid - 1
+//   because mid itself may be the peak.
+// 
+// • There can be multiple peaks.
+//   Returning any one is valid.
 
 class Solution {
     public int findPeakElement(int[] nums) {
