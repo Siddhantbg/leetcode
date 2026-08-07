4ae7750f39bb1f6cd702cf659335cac5a4d467ed
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-08-08 02:05:02 +0530

    LeetCode: Solve #4 Median of Two Sorted Arrays

--- LeetCode/Hard/Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.java
+++ LeetCode/Hard/Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.java
@@ -1,8 +1,41 @@
 // 4. Median of Two Sorted Arrays [Hard]
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 // Language: java | Runtime: 1 ms | Memory: 48.9 MB
+// Time:  O(log(min(n1,n2)))
+// Space: O(1)
 // Tags: Array, Binary Search, Divide and Conquer
 // Synced: 2026-08-08
+//
+// • Binary Search on the SMALLER array.
+// 
+// • Partition both arrays such that:
+//     Left Half = Right Half
+//     (or Left has one extra element for odd total size)
+// 
+// • Four important values:
+//     l1, r1, l2, r2
+// 
+// • Correct partition when:
+//     l1 <= r2
+//     AND
+//     l2 <= r1
+// 
+// • Even total elements:
+//     Median = (max(l1,l2) + min(r1,r2)) / 2
+// 
+// • Odd total elements:
+//     Median = max(l1,l2)
+// 
+// • If l1 > r2:
+//     Move LEFT.
+//     (high = cut1 - 1)
+// 
+// • Else:
+//     Move RIGHT.
+//     (low = cut1 + 1)
+// 
+// • Always Binary Search on the smaller array to achieve
+//     O(log(min(n,m))) time.
 
 class Solution {
 
