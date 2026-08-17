0e6ba1759f1cec321b73f6237452a8c4890761fa
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-08-17 23:49:18 +0530

    LeetCode: Solve #540 Single Element in a Sorted Array

--- LeetCode/Medium/Single Element in a Sorted Array/540. Single Element in a Sorted Array.java
+++ LeetCode/Medium/Single Element in a Sorted Array/540. Single Element in a Sorted Array.java
@@ -1,6 +1,6 @@
 // 540. Single Element in a Sorted Array [Medium]
 // https://leetcode.com/problems/single-element-in-a-sorted-array/
-// Language: java | Runtime: 0 ms | Memory: 52.8 MB
+// Language: java | Runtime: 0 ms | Memory: 52.9 MB
 // Tags: Array, Binary Search
 // Synced: 2026-08-17
 
@@ -8,23 +8,40 @@ class Solution {
 
     public int singleNonDuplicate(int[] nums) {
 
-        int low = 0;
-        int high = nums.length - 1;
+        int n = nums.length;
 
-        while (low < high) {
+        if (n == 1) return nums[0];
+
+        if (nums[0] != nums[1]) return nums[0];
+
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high) {
 
             int mid = low + (high - low) / 2;
 
-            if (mid % 2 == 1)
-                mid--;
+            // Single element found
+            if (nums[mid] != nums[mid - 1] &&
+                nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
 
-            if (nums[mid] == nums[mid + 1]) {
-                low = mid + 2;
-            } else {
-                high = mid;
+            // Pattern is correct, move right
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+                (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+
+                low = mid + 1;
+            }
+
+            // Pattern broken, move left
+            else {
+                high = mid - 1;
             }
         }
 
-        return nums[low];
+        return -1;
     }
 }
