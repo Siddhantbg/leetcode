a2ba1ed9b4881e20b31d71ab5465962cc2725006
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-08-20 02:50:31 +0530

    LeetCode: Solve #9 Palindrome Number

---
+++ LeetCode/Easy/Palindrome Number/optimal/9. Palindrome Number.java
@@ -0,0 +1,23 @@
+// 9. Palindrome Number [Easy]
+// https://leetcode.com/problems/palindrome-number/
+// Language: java | Runtime: 5 ms | Memory: 45.5 MB
+// Time:  O(logn)
+// Space: O(1)
+// Tags: Math
+// Synced: 2026-08-20
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return original == reverse;
+    }
+}
