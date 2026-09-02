8c97796f1884bab39311d7f68e86204f1282e06b
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-09-03 01:04:10 +0530

    LeetCode: Solve #1 Two Sum

--- LeetCode/Easy/Two Sum/1. Two Sum.java
+++ LeetCode/Easy/Two Sum/1. Two Sum.java
@@ -1,22 +1,22 @@
 // 1. Two Sum [Easy]
 // https://leetcode.com/problems/two-sum/
-// Language: java | Runtime: 2 ms | Memory: 47.1 MB
-// Time:  O(n)
-// Space: O(n)
+// Language: java | Runtime: 3 ms | Memory: 47.1 MB
 // Tags: Array, Hash Table
-// Synced: 2026-06-30
+// Synced: 2026-09-03
 
-class Solution{
-    public int[] twoSum(int nums[],int target){
-        HashMap<Integer,Integer> map=new HashMap<>();
-        
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+
         for(int i=0;i<nums.length;i++){
-            int need=target-nums[i];
-            if(map.containsKey(need)){
-                return new int[]{map.get(need),i};
+            int complement=target-nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
             map.put(nums[i],i);
-        }
+        }   
         return new int[]{-1,-1};
     }
 }
