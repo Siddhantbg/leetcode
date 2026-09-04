8864fb93aa6be810537233e254a39dde26abf495
Sid <156163973+Siddhantbg@users.noreply.github.com>
2026-09-05 02:12:26 +0530

    LeetCode: Solve #162 Find Peak Element

--- LeetCode/Medium/Find Peak Element/162. Find Peak Element.java
+++ LeetCode/Medium/Find Peak Element/162. Find Peak Element.java
@@ -1,50 +1,26 @@
 // 162. Find Peak Element [Medium]
 // https://leetcode.com/problems/find-peak-element/
-// Language: java | Runtime: 0 ms | Memory: 44.2 MB
-// Time:  O(log n)
-// Space: O(1)
+// Language: java | Runtime: 0 ms | Memory: 43.9 MB
 // Tags: Array, Binary Search
-// Synced: 2026-08-07
-//
-// • Binary Search on the slope.
-// 
-// • Compare only:
-//     nums[mid] and nums[mid + 1].
-// 
-// • If nums[mid] < nums[mid + 1]:
-//     You're on an increasing slope.
-//     Search RIGHT.
-// 
-// • If nums[mid] > nums[mid + 1]:
-//     You're on a decreasing slope.
-//     Search LEFT (including mid).
-// 
-// • Use:
-//     while (low < high)
-// 
-// • When low == high,
-//     that index is guaranteed to be a peak.
-// 
-// • Don't use:
-//     high = mid - 1
-//   because mid itself may be the peak.
-// 
-// • There can be multiple peaks.
-//   Returning any one is valid.
+// Synced: 2026-09-05
 
 class Solution {
     public int findPeakElement(int[] nums) {
-        int l=0,h=nums.length-1;
+        int low = 0;
+        int high = nums.length - 1;
 
-        while(l<h){
-            int mid=l+(h-l)/2;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
 
-            if(nums[mid]<nums[mid+1]){
-                l=mid+1;
-            }else{
-                h=mid;
+            if (nums[mid] < nums[mid + 1]) {
+                // Going UP → peak is on the right
+                low = mid + 1;
+            } else {
+                // Going DOWN → peak is at mid or on the left
+                high = mid;
             }
         }
-        return l;
+
+        return low;
     }
 }
